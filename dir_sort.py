d09742d4c6121a4f84c313760284ff54161e18af
Anthony Smith <asmith@cari.net>
2016-01-22 23:43:20 -0800

    This will sort and return the path of the most recent file matching regular expression.

---
+++ dir_sort.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+# This class returns file paths for a directory. Can be used for either oldest/newest file in a directory.
+
+import os
+import time
+import glob
+
+class sort(object):
+	def __init__(self):
+		pass
+
+	def recent(self, path, name):
+		self.path = path
+		self.name = name
+		args = ("%s%s" % (path, name))
+		file_list = glob.glob(args)
+		return self.comp_high(file_list)
+
+	def comp_high(self, file_list):
+		self.file_list = file_list
+		self.time = 0
+		self.path = None
+		for path in self.file_list:
+			if (os.path.getctime(path) > self.time) is True:
+				self.time = os.path.getctime(path)
+				self.path = path
+		return self.path
