f01e2ba5ebe8c472bbccb4358a501b9bbe94dd23
Anthony Smith <asmith@cari.net>
2016-01-23 00:00:44 -0800

    Added function to return oldest log in folder.

--- dir_sort.py
+++ dir_sort.py
@@ -10,19 +10,36 @@ class sort(object):
 	def __init__(self):
 		pass
 
-	def recent(self, path, name):
-		self.path = path
-		self.name = name
-		args = ("%s%s" % (path, name))
-		file_list = glob.glob(args)
-		return self.comp_high(file_list)
+	def comp_new(self, file_list):
+                self.file_list = file_list
+                self.time = 0
+                self.path = None
+                for path in self.file_list:
+                        if (os.path.getctime(path) > self.time) is True:
+                                self.time = os.path.getctime(path)
+                                self.path = path
+                return self.path
 
-	def comp_high(self, file_list):
+	def comp_old(self, file_list):
 		self.file_list = file_list
 		self.time = 0
 		self.path = None
 		for path in self.file_list:
-			if (os.path.getctime(path) > self.time) is True:
+			if (os.path.getctime(path) < self.time) is True:
 				self.time = os.path.getctime(path)
 				self.path = path
 		return self.path
+
+	def newest(self, path, name):
+		self.path = path
+		self.name = name
+		args = ("%s%s" % (path, name))
+		file_list = glob.glob(args)
+		self.comp_new(file_list)
+	
+	def oldest(self, path, name):
+		self.path = path
+		self.name = name
+		args = ("%s%s" % (path, name))
+		file_list = glob.glob(args)
+		self.comp_old(file_list)
