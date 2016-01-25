4bc56dfc0695ec79d99d3e5a7cdd1872eade098e
Anthony Smith <asmith@cari.net>
2016-01-25 10:49:19 -0800

    Removed self. from all incorrectly scoped variables.

--- log_parse.py
+++ log_parse.py
@@ -12,21 +12,21 @@ class log_parse(object):
 		self.init_check = 0
 
 	def readout(self, path):
-		self.path = path
-		self.ofile = codecs.open(('%s' % self.path), 'r', encoding='utf-16-le')
-		self.ofile.seek(self.offset)
-		if (self.ofile.readlines() == []) is False:
-			self.ofile.seek(self.offset)
-			self.append = self.ofile.readlines()
+		path = path
+		ofile = codecs.open(('%s' % path), 'r', encoding='utf-16-le')
+		ofile.seek(self.offset)
+		if (ofile.readlines() == []) is False:
+			ofile.seek(self.offset)
+			append = ofile.readlines()
 			if (self.init_check == 0) is False:
-				self.text = self.text + self.append
-				for line in self.append:
+				self.text = self.text + append
+				for line in append:
 					line = line.encode("utf-8")
 					self.log_array.append(self.line_parse(line))
 			else:
-				self.text = self.append
-			self.offset = self.ofile.tell() + 2
-		self.ofile.close()
+				self.text = append
+			self.offset = ofile.tell() + 2
+		ofile.close()
 
 	def get_start(self):
 		if (self.log_array[0][1] == 'EVE System') is True:
@@ -39,16 +39,16 @@ class log_parse(object):
 			self.log_array.append(self.line_parse(line))
 
 	def line_parse(self, line):
-		self.line = line
-		self.timestamp = self.line[self.line.find('['):self.line.find('[')+23]
-		self.name = self.line[self.line.find('[')+24:self.line.find('>')-1]
-		self.message = self.line[self.line.find('>')+2:self.line.find('\r')]
-		self.line_array = [self.timestamp, self.name, self.message]
-		return self.line_array
+		line = line
+		timestamp = line[line.find('['):line.find('[')+23]
+		name = line[line.find('[')+24:line.find('>')-1]
+		message = line[line.find('>')+2:line.find('\r')]
+		line_array = [timestamp, name, message]
+		return line_array
 
 	def log_parse(self, path):
-		self.path = path
-		self.readout(self.path)
+		path = path
+		self.readout(path)
 		if (self.init_check == 0) is True:
 			self.init_parse()
 		self.get_start()
