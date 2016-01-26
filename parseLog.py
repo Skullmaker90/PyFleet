7d6e60f6bf3ed6dba52c612a8a6c60d4c09a4c82
Anthony Smith <asmith@cari.net>
2016-01-26 07:47:09 -0800

    This didn't need to be a class.

--- parseLog.py
+++ parseLog.py
@@ -2,29 +2,20 @@
 
 # This class is used to open utf-16-le text documents and parse them accodingly.
 
-import os, codecs, time
+def line_parse(self, line):
+	line = line.encode('utf-8')
+	timestamp = line[line.find('['):line.find('[')+23]
+	name = line[line.find('[')+24:line.find('>')-1]
+	message = line[line.find('>')+2:line.find('\r')]
+	line_array = [timestamp, name, message]
+	return line_array
 
-class log_parse(object):
-	def __init__(self):
-		self.text = []
-		self.log_array = []
-		self.lolen = 0
-		self.tlen = 0
-
-	def cut_log(self):
-		if (self.log_array[0][12] == 'EVE System') is True:
-			self.log_array = self.log_array[13:self.lolen]
-
-	def line_parse(self, line):
-		line = line.encode('utf-8')
-		timestamp = line[line.find('['):line.find('[')+23]
-		name = line[line.find('[')+24:line.find('>')-1]
-		message = line[line.find('>')+2:line.find('\r')]
-		line_array = [timestamp, name, message]
-		return line_array
-
-	def log_parse(self, text):
-		for line in text:
-			line = self.line_parse(line)
-			self.log_array.append(line)
-		self.cut_log()
+def log_parse(self, log):
+	text = text
+	log = []
+	for line in log:
+		line = line_parse(line)
+		log.append(line)
+	if (log[0][12] == 'EVE System') is True:
+		log = log[13:len(log)]
+	return log
