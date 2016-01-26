4ea80851e95c1265da4a3c84dc656c4944ce65e3
Anthony Smith <asmith@cari.net>
2016-01-26 14:29:30 -0800

    Fixed parser to correctly remove top section of log.

--- parseLog.py
+++ parseLog.py
@@ -2,7 +2,7 @@
 
 # This class is used to open utf-16-le text documents and parse them accodingly.
 
-def line_parse(self, line):
+def line_parse(line):
 	line = line.encode('utf-8')
 	timestamp = line[line.find('['):line.find('[')+23]
 	name = line[line.find('[')+24:line.find('>')-1]
@@ -10,12 +10,14 @@ def line_parse(self, line):
 	line_array = [timestamp, name, message]
 	return line_array
 
-def log_parse(self, log):
-	text = text
+def log_parse(log):
+	text = log
 	log = []
-	for line in log:
+	for line in text:
 		line = line_parse(line)
 		log.append(line)
-	if (log[0][12] == 'EVE System') is True:
-		log = log[13:len(log)]
+	if (log[12][2] != 'EVE System') is False:
+		log = log[12:len(log)]
+		return log
+	log = log[13:len(log)]
 	return log
