d564bea6a9aa82bc59a50f0b0693c19e1aef3ac9
Anthony Smith <asmith@cari.net>
2016-01-25 20:28:57 -0800

    New readLog module, takes path and outputs utf-16-le log as array.

---
+++ readLog.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+class log(object):
+	def __init__(self, path):
+		self.log = None
+		self.ofile = None
+		self.path = path
+		self.offset = 0
+
+	def open(self):
+		try:
+			self.ofile = codecs.open(('%s' % self.path), 'r', encoding='utf-16-le')
+		except IOError:
+			print("Could not open file.")
+			
+	def acheck(self):
+		self.ofile.seek(self.offset)
+		x = (self.ofile.readlines() == [])
+		self.ofile.seek(self.offset)
+		return x
+
+	def append(self):
+		if self.acheck():
+			break
+		else:
+			self.log = self.log + self.ofile.readlines()
+			self.offset = self.offset + self.ofile.tell() + 2
+		return self.log
